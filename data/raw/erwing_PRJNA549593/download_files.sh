8423eb56723f868f5620a377e4eff53978cf9508
andres <andresgordoortiz@gmail.com>
2026-01-08 17:08:26 +0100

    Cleaning non needed files

--- data/raw/erwing_PRJNA549593/download_files.sh
+++
@@ -1,8 +0,0 @@
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/004/SRR9326194/SRR9326194_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/001/SRR9326191/SRR9326191_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/002/SRR9326192/SRR9326192_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/003/SRR9326193/SRR9326193_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/004/SRR9326194/SRR9326194_2.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/001/SRR9326191/SRR9326191_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/003/SRR9326193/SRR9326193_1.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR932/002/SRR9326192/SRR9326192_1.fastq.gz
